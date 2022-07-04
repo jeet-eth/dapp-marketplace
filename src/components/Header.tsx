ff2a41c37390f56b3b1f2755419cc1ccdfc1ddc8
jeet <simranjeet@colexion.io>
2022-07-04 17:26:52 +0530

    fix: remove theme to fix hydration errors

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -9,18 +9,18 @@ import { ChainId } from "./ChainId";
 
 export const Header = function () {
   const { active, error } = useWeb3React();
-  const [theme, setTheme] = useLocalStorage<"dark" | "light">("theme", "dark");
+  // const [theme, setTheme] = useLocalStorage<"dark" | "light">("theme", "dark");
 
-  useEffect(() => {
-    document.documentElement.setAttribute("data-theme", theme);
-  }, []);
+  // useEffect(() => {
+  //   document.documentElement.setAttribute("data-theme", theme);
+  // }, []);
 
-  const toggleTheme = () => {
-    setTheme((prevTheme) => {
-      document.documentElement.setAttribute("data-theme", prevTheme === "dark" ? "light" : "dark");
-      return prevTheme === "dark" ? "light" : "dark";
-    });
-  };
+  // const toggleTheme = () => {
+  //   setTheme((prevTheme) => {
+  //     document.documentElement.setAttribute("data-theme", prevTheme === "dark" ? "light" : "dark");
+  //     return prevTheme === "dark" ? "light" : "dark";
+  //   });
+  // };
 
   return (
     <div className="mb-2 shadow-lg navbar bg-neutral text-neutral-content rounded-box">
@@ -32,9 +32,9 @@ export const Header = function () {
           <ChainId />
           <Account />
           <Balance />
-          <button type="button" onClick={toggleTheme} className="btn ">
+          {/* <button type="button" onClick={toggleTheme} className="btn ">
             {theme === "dark" ? "🌞" : "🌙"}
-          </button>
+          </button> */}
         </div>
       </div>
     </div>
