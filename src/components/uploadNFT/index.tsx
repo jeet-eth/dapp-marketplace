b4a12f1b5b2f3c70f28d0f2b65b0bf4b8427d22e
jeet <simranjeet@colexion.io>
2022-07-05 17:22:54 +0530

    feat: upload File to IPFS

--- src/components/uploadNFT/index.tsx
+++ src/components/uploadNFT/index.tsx
@@ -1,27 +1,80 @@
 import { useState } from "react";
 
+import useIpfs from "../../hooks/useIpfs";
+
 function UploadNFT() {
   const [name, setName] = useState("");
   const [description, setDescription] = useState("");
   const [file, setFile] = useState(null);
-  const [ipfsHash, setIpfsHash] = useState(null);
+  const [ipfsUrl, setIpfsUrl] = useState("");
+
+  const [uiState, setUiState] = useState({
+    ipfsLoading: false,
+    ipfsMessage: null,
+    ipfsError: null,
+    mintLoading: false,
+    mintError: null,
+  });
+
+  const { uploadFile } = useIpfs();
 
   const captureFile = (event: any) => {
     event.stopPropagation();
     event.preventDefault();
     const file = event.target.files[0];
-    setFile(file);
+    const reader = new window.FileReader();
+    reader.readAsArrayBuffer(file);
+    reader.onloadend = () => {
+      setFile(Buffer.from(reader.result as any));
+    };
   };
 
-  const uploadNFT = (e: any) => {
-    console.log(e);
+  const mintNFT = (event: any) => {
+    event.stopPropagation();
+    event.preventDefault();
+
+    const metadata = {
+      name,
+      description,
+      image: ipfsUrl,
+    };
+
+    console.log(metadata);
   };
-  const uploadFile = (e: any) => {
-    console.log(e);
+
+  const uploadImgToIPFS = async (e: any) => {
+    if (file) {
+      setUiState((p) => ({
+        ...p,
+        ipfsError: null,
+        ipfsLoading: true,
+      }));
+      try {
+        const url = await uploadFile(file);
+        setIpfsUrl(url);
+      } catch (err) {
+        setUiState((p) => ({
+          ...p,
+          ipfsError: `Error uploading file: ${err}`,
+        }));
+      }
+
+      setUiState((p) => ({
+        ...p,
+        ipfsError: null,
+        ipfsMessage: "File has been uploaded to IPFS",
+        ipfsLoading: false,
+      }));
+    } else {
+      setUiState((p) => ({
+        ...p,
+        ipfsError: "Choose a File to Upload",
+      }));
+    }
   };
 
   return (
-    <div className="form-control flex flex-col gap-4 w-2/3">
+    <form onSubmit={mintNFT} className="form-control flex flex-col gap-4 w-2/3">
       <div>
         <label className="label">
           <span className="label-text">Enter NFT Name</span>
@@ -31,6 +84,7 @@ function UploadNFT() {
           <input
             required
             type="text"
+            value={name}
             placeholder="eg. CryptoPunks"
             className="input input-bordered w-full"
             onChange={(e) => setName(e.target.value)}
@@ -46,6 +100,7 @@ function UploadNFT() {
           <input
             required
             type="text"
+            value={description}
             placeholder="eg. My First NFT"
             className="input input-bordered w-full"
             onChange={(e) => setDescription(e.target.value)}
@@ -56,14 +111,52 @@ function UploadNFT() {
         <label className="label">
           <span className="label-text">Upload NFT File</span>
         </label>
-        <label className="input-group w-min ">
+        <label className="input-group w-min">
           <span>File</span>
-          <input type="file" className="mr-12" onChange={captureFile} accept="image/png, image/jpeg" />
-          <button onClick={uploadFile} className="btn normal-case btn-sm btn-accent">
-            Upload NFT to IPFS
-          </button>
+          <input
+            disabled={uiState.ipfsLoading}
+            type="file"
+            className="mr-12"
+            onChange={captureFile}
+            accept="image/png, image/jpeg"
+          />
+          {uiState.ipfsLoading ? (
+            <button type="button" disabled className="btn normal-case btn-sm loading btn-accent">
+              Uploading Image to IPFS
+            </button>
+          ) : (
+            <button type="button" onClick={uploadImgToIPFS} className="btn normal-case btn-sm btn-accent">
+              Upload Image to IPFS
+            </button>
+          )}
         </label>
       </div>
+      {uiState.ipfsError && <div className="text-red-500 -mb-5">{uiState.ipfsError}</div>}
+      {uiState.ipfsMessage && (
+        <div className="flex gap-5 items-center text-green-500 -mb-5">
+          <p className="flex gap-3">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="stroke-current flex-shrink-0 h-6 w-6"
+              fill="none"
+              viewBox="0 0 24 24"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"
+              />
+            </svg>
+            {uiState.ipfsMessage}
+          </p>
+          {ipfsUrl && (
+            <a href={ipfsUrl} target="_blank" className="btn btn-sm normal-case">
+              Open File
+            </a>
+          )}
+        </div>
+      )}
 
       <div className="divider">OR</div>
 
@@ -76,17 +169,18 @@ function UploadNFT() {
           <input
             required
             type="text"
-            placeholder="eg. ipfs://bafybeidz6dftb2jjyfi4sempbuwlbazod56nkvcubth7i5bkpunkayrkt4"
+            placeholder="eg. https://ipfs.infura.io/ipfs/bafybeidz6dftb2jjyfi4sempbuwlbazod56nkvcubth7i5bkpunkayrkt4"
             className="input input-bordered w-full"
-            onChange={(e) => setIpfsHash(e.target.value)}
+            value={ipfsUrl}
+            onChange={(e) => setIpfsUrl(e.target.value)}
           />
         </label>
       </div>
 
-      <button onClick={uploadNFT} className="btn btn-wide btn-primary my-12 mx-auto">
-        Upload NFT
+      <button type="submit" className="btn btn-wide btn-primary my-12 mx-auto">
+        Mint NFT
       </button>
-    </div>
+    </form>
   );
 }
 
---
+++ src/hooks/useIpfs.ts
@@ -0,0 +1,24 @@
+import { create } from "ipfs-http-client";
+import { ImportCandidate } from "ipfs-core-types/src/utils";
+
+const client = create({ url: "https://ipfs.infura.io:5001/api/v0" });
+
+const useIpfs = () => {
+  const uploadFile = async (file: ImportCandidate) => {
+    try {
+      const added = await client.add(file);
+      const url = `https://ipfs.infura.io/ipfs/${added.path}`;
+
+      return url;
+    } catch (error) {
+      console.error("Error uploading file: ", error);
+      return error
+    }
+  };
+
+  return {
+    uploadFile,
+  };
+};
+
+export default useIpfs;
